c9c3be3189bed2b8f5374974804959264d834453
SayantanDs <sayantanapril99@gmail.com>
2020-05-19 21:10:24 +0530

    cleanup

--- Game/settings.py
+++ Game/settings.py
@@ -1,3 +1,7 @@
+import os
+from .utils import resource_path
+
+
 # General Settings
 SCREEN_HEIGHT = 480
 SCREEN_WIDTH = int(SCREEN_HEIGHT * 16 / 9)
@@ -6,7 +10,7 @@ FINAL_HEIGHT = 480
 FINAL_WIDTH = int(FINAL_HEIGHT * 16 / 9)
 
 FULLSCREEN = False
-SHOW_FPS = True
+SHOW_FPS = False
 
 WIN_TITLE = "Tim's Adventures"
 FPS = 60
@@ -15,11 +19,11 @@ FPS = 60
 jump_time = 0.75
 jump_height = 64+16
 t = jump_time/2
-GRAVITY = int((2*jump_height)/(t*t))         # 640
-PLAYER_JUMP_SPEED = int((2*jump_height)/t)   # 320
+GRAVITY = int((2*jump_height)/(t*t))
+PLAYER_JUMP_SPEED = int((2*jump_height)/t)
 PLAYER_WALK_SPEED = int(32*6/jump_time)
 
-
+# terminal velocity
 TERMINAL_V = 700
 
 MAX_COLLISION_STEP_SIZE = 8
@@ -30,9 +34,6 @@ DEBUG_DRAW = False
 DEBUG_DRAW_COLOR = (255, 0, 255)
 
 # Resource Folders
-import os
-from .utils import resource_path
-
 resource_folder = resource_path('Resources')
 img_folder = os.path.join(resource_folder, 'images')
 music_folder = os.path.join(resource_folder, 'music')
